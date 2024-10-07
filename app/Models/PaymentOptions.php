4abdb8b3003ecbe69377f1c47c6ee1bddf8b21d4
Ritik Rock <138879772+ritzritik@users.noreply.github.com>
2024-10-08 03:00:27 +0530

    Added Payment gateway

--- app/Models/PaymentOptions.php
+++ app/Models/PaymentOptions.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentOptions extends Model
 {
     use HasFactory;
+
+    protected $table = 'payment_options';
+
+    protected $fillable = [
+        'id',
+        'payment_mode'
+    ];
+
 }
